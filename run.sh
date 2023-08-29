83ae82ec4687683e17b28b8fe8711147e5127d32
Vedran Kasalica <v.kasalica@esciencecenter.nl>
2023-08-29 07:05:27 +0200

    Minor fixes

--- run.sh
+++ run.sh
@@ -14,6 +14,10 @@ fi
 # Execute the initial workflow - DONE
 #docker run -v ./data:/data  workflomics/container:v1.0 cwl-runner --leave-tmpdir --outdir /data/output /data/examples/No\ 0/workflow.cwl /data/examples/No\ 0/input.yml
 
+# Execute the test workflow - DONE
+docker run -v ./data:/data  workflomics/container:v1.0 cwl-runner --leave-tmpdir --outdir /data/output /data/workflows/my2/workflowSolution.cwl /data/workflows/my2/input.yml
+
+
 # Execute No1 - DONE
 # docker run -v ./data:/data  workflomics/container:v1.0 cwl-runner --leave-tmpdir --outdir /data/output /data/examples/No\ 1/workflowSolution.cwl /data/examples/No\ 1/input.yml
 
@@ -24,10 +28,7 @@ fi
 #docker run -v ./data:/data  workflomics/container:v1.0 cwl-runner --leave-tmpdir --outdir /data/output /data/examples/No\ 3/workflowSolution.cwl /data/examples/No\ 3/input.yml
 
 # Execute No4 - XTandem1 nor gProfiler NOT AVAILABLE
- #docker run -v ./data:/data  workflomics/container:v1.0 cwl-runner --leave-tmpdir --outdir /data/output /data/examples/No\ 4/workflowSolution.cwl /data/examples/No\ 4/input.yml
+# docker run -v ./data:/data  workflomics/container:v1.0 cwl-runner --leave-tmpdir --outdir /data/output /data/examples/No\ 4/workflowSolution.cwl /data/examples/No\ 4/input.yml
 
 # Execute No5 - XTandem1 NOT AVAILABLE
 # docker run -v ./data:/data  workflomics/container:v1.0 cwl-runner --leave-tmpdir --outdir /data/output /data/examples/No\ 5/workflowSolution.cwl /data/examples/No\ 5/input.yml
-
-# Execute No6 - XTandem1 NOT AVAILABLE
-# docker run -v ./data:/data  workflomics/container:v1.0 cwl-runner --leave-tmpdir --outdir /data/output /data/examples/No\ 6/workflowSolution.cwl /data/examples/No\ 6/input.yml
